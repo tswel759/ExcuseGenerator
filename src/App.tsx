4b5d4b093add6f9ca8d1448216a620359eb94cf3
szyrad <@dfd>
2025-04-02 15:11:07 +0200

    zmiany

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
-import { useState } from 'react'
-import './App.css'
+import{useState} from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+interface FormData{
+  name: string;
+  powod_wymowki: string;
+}
 
-  return (
-    <>
+const Form =() =>{
+  const [formData,setFormData]=useState<FormData>({name:"",powod_wymowki:""});
 
-    </>
+  function handleSubmit(e: React.FormEvent){
+    console.log("nacisnąłeś submita")
+    console.log(e)
+  }
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>)
+  {
+    console.log(e)
+    const{name,value}=e.target;
+    setFormData(prev=>({...prev, [name]:value}))
+    console.log(formData)
+  }
+  return(
+      <form onSubmit={handleSubmit}>
+        <label>
+          Imię:
+          <input type="text"
+                 name="name"
+                 value={formData.name}
+                 onChange={handleChange}
+          />
+        </label>
+        <label>
+          Powod wymowki:
+          <input type="select"
+                 name="powod_wymowki"
+                 value={formData.powod_wymowki}
+                 onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Wyslij formularz</button>
+      </form>
   )
 }
 
-export default App
+export default Form;
